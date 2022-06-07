f3fb209bc830929669dcc370973902fc92d954c5
Pius Githugu <cehgithugu@gmail.com>
2022-06-07 14:46:45 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,9 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+let wordBlanks = "Today I saw a " + myNoun + " interestingly very " + myAdjective + ". ";
+wordBlanks += "It " + myVerb + " " + myAdverb + " to attack me.";// Change this line
+// Only change code above this line
