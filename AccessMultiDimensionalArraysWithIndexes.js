ec1f7b44137e6ccdfb58d137055c890b1760c7dc
Pius Githugu <cehgithugu@gmail.com>
2022-06-08 12:27:10 +0300

    code refactor

--- AccessMultiDimensionalArraysWithIndexes.js
+++ AccessMultiDimensionalArraysWithIndexes.js
@@ -91,3 +91,77 @@ const testArr = [1, 2, 3, 4, 5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+
+// 
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val <10 || val>20) {
+    return "Outside";
+  }
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+// 
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+// 
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else{
+    return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);
+
+// 
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return 'Tiny';
+  }
+  else if (num < 10) {
+    return 'Small';
+  }
+  else if (num < 15) {
+    return 'Medium';
+  }
+  else if (num < 20) {
+    return 'Large';
+  }
+  else {
+    return 'Huge';
+  }
+  // Only change code above this line
+}
+
+testSize(7);
+
+// 
