51126aa94a3a6fd917a3b7160b943b701a4193b0
Pius Githugu <cehgithugu@gmail.com>
2022-06-09 13:07:02 +0300

    golf solution

--- golf.js
+++ golf.js
@@ -14,3 +14,35 @@ par + 2	"Double Bogey"
 par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
 
  */
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes ===1) {
+    return "Hole-in-one!";
+  }
+  else if (strokes === 2) {
+    return "Eagle";
+}
+  else if (strokes === 3) {
+    return "Birdie";
+} 
+  else if (par === 4 && strokes === 4) {
+    return "Par";
+} 
+  else if (par ===4 && strokes ===5) {
+    return "Bogey";
+} 
+  else if (par ===4 && strokes ===6) {
+    return "Double Bogey";
+} 
+  else if (par >=4 && strokes >=7) {
+    return "Go Home!";
+} 
+  else if (par ===5 && strokes ===5) {
+    return "Par";
+}
+  // Only change code above this line
+}
+
+golfScore(5, 4);
