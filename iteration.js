fa44af8a09f41284ddc25a944b05d5dbe91a5a91
Githugu Pius <cehgithugu@gmail.com>
2022-07-12 17:30:16 +0300

    Update iteration.js

--- iteration.js
+++ iteration.js
@@ -11,3 +11,19 @@ while (i >= 0) {
 }
 
 // Only change code below this line
+
+
+
+
+
+Iterate Through an Array with a For Loop
+Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+
+// Only change code below this line
+for (var i = 0; i < myArr.length; i++) {
+total += myArr [i];
+}
